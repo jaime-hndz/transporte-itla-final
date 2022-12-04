a0a47370b9ab16db9905a5be808fc5c6f04653fa
Jaime <jaime251102@gmail.com>
2022-12-04 12:17:36 -0400

    cambiando las rutas en el mapa

--- transporte-itla/src/components/RoutesMap.js
+++ transporte-itla/src/components/RoutesMap.js
@@ -1,19 +1,73 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
+import React, { useState, useEffect } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Polyline,
+} from "react-leaflet";
 import * as L from "leaflet";
-
+import { useMap } from "react-leaflet";
 import { WeekSchedule } from "../data/ScheduleData";
+import { Button } from "@mui/material";
 
 export const RoutesMap = () => {
-  const position = [18.45131484797611, -69.66274166572548];
+  const itla = [18.45131484797611, -69.66274166572548];
+  const [ver, setVer] = useState(false);
+  
+
+  function LocationMarker({ ver = false }) {
+    const [position, setPosition] = useState(null);
+
+    const map = useMap();
+
+    const ubicacion = () => {
+      map.locate().on("locationfound", function (e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+        const radius = e.accuracy;
+        const circle = L.circle(e.latlng, radius);
+        circle.addTo(map);
+        // setBbox(e.bounds.toBBoxString().split(","));
+      });
+    };
+
+    useEffect(() => {
+      if (ver) {
+        ubicacion();
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [ver]);
+
+    return position === null ? null : (
+      <Marker position={position} icon={new L.Icon({
+        iconUrl:
+          "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+        shadowUrl:
+          "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41],
+      })}>
+        <Popup>Estas aquí</Popup>
+      </Marker>
+    );
+  }
+
   return (
     <div>
-      <MapContainer className="leaflet-container" center={position} zoom={15}>
+        <div style={{display:'flex'}}>
+      <Button onClick={() => setVer(true)}>Ver tu ubicacion en el mapa</Button>
+        </div>
+            
+      <MapContainer className="leaflet-container" center={itla} zoom={15}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>  | <a href="https://www.google.com/maps/d/u/2/viewer?mid=10bNTYESub5N0jc6x7kYPiUkrKL2ypCAX&ll=18.462327684817264%2C-69.8181437515895&z=13">Ver en google maps</a> '
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
+        <LocationMarker ver={ver} />
+        <Marker position={itla}>
           <Popup>Instituto Tecnológico de Las Américas</Popup>
         </Marker>
         {WeekSchedule.map((ruta, i) => (
@@ -24,15 +78,25 @@ export const RoutesMap = () => {
               color={ruta.color}
             />
 
-            {ruta.paradas.map((parada) => (
-              <Marker icon={new L.Icon({
-                iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-'+ruta.color+'.png',
-                shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-                iconSize: [25, 41],
-                iconAnchor: [12, 41],
-                popupAnchor: [1, -34],
-                shadowSize: [41, 41]
-              })} position={parada.ubicacion}>
+            {ruta.paradas.map((parada,i) => (
+              <Marker
+                key={i}
+                icon={
+                  new L.Icon({
+                    iconUrl:
+                      "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-" +
+                      ruta.color +
+                      ".png",
+                    shadowUrl:
+                      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+                    iconSize: [25, 41],
+                    iconAnchor: [12, 41],
+                    popupAnchor: [1, -34],
+                    shadowSize: [41, 41],
+                  })
+                }
+                position={parada.ubicacion}
+              >
                 <Popup>{parada.title}</Popup>
               </Marker>
             ))}
