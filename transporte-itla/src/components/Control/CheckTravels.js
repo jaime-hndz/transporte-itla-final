82f3d242f40835195d34e0ca117ae1f1d7fbaf65
Jaime <jaime251102@gmail.com>
2022-12-09 23:18:56 -0400

    qr reader funcionando

--- transporte-itla/src/components/Control/CheckTravels.js
+++ transporte-itla/src/components/Control/CheckTravels.js
@@ -1,20 +1,41 @@
 import React, { useState } from 'react';
 import { QrReader } from 'react-qr-reader';
+import {fetchApi} from '../../helpers/fetchApi'
 
 export const CheckTravels = ({viaje}) => {
 
+  const previewStyle = {
+    height: 240,
+    width: 320
+  };
+
+  const check = async (estudiante) =>{
+    await fetchApi(`tickets/check/${viaje.idViaje}/${estudiante}`, null, 'GET')
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((error) => {
+    console.log(error);
+    })
+  }
+
   const [data, setData] = useState('No result');
   return (
-    <>
-      <QrReader
-        onResult={(result, error) => {
-          if (!!result) {
-            setData(result?.text);
-          }
-        }}
-        style={{ width: '500px' }}
-      />
-      <p>{data}</p>
-    </>
+    <div>
+      <div style={{width: '500px', marginInline: 'auto'}}>
+        <QrReader
+          onResult={(result, error) => {
+            if (!!result) {
+              setData(result?.text);
+              check(result)
+            }
+          }}
+          style={previewStyle}
+          />
+      </div>
+      <div style={{width: '500px', marginInline: 'auto'}}>
+        <p>{data}</p>
+      </div>
+    </div>
   )
 }
